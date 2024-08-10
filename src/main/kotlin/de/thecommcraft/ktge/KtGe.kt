3dd6c3bc709485bfed77913307f13ec57a7cbf50
gustav-langer <ich.leake.nicht@meine.email.adresse>
2024-08-10 20:55:06 +0200

    Improved Sprite system, rewrote TemplateProgram.kt using it in Main.kt

--- src/main/kotlin/de/thecommcraft/ktge/KtGe.kt
+++ src/main/kotlin/de/thecommcraft/ktge/KtGe.kt
@@ -3,6 +3,9 @@ package de.thecommcraft.ktge
 import org.openrndr.*
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.ColorBuffer
+import org.openrndr.draw.Drawer
+import org.openrndr.draw.FontMap
+import org.openrndr.draw.loadImage
 import org.openrndr.math.Vector2
 
 // Demo application. Press ESCAPE to exit.
@@ -40,33 +43,144 @@ val demoBuilder: ApplicationBuilder.() -> Unit = {
     }
 }
 
-typealias Builder<T> = T.() -> Unit
+interface Builder<T> {
+    fun build(): T
+}
 
-data class SpriteState(val position: Vector2)
+typealias BuildFun<T> = T.() -> Unit
+
+data class SpriteState(val position: Vector2, val size: Double)
+
+interface Drawable {
+    fun draw(program: Program)
+}
+/*abstract class PreDrawable: Drawable {
+    private var _preDraw: Drawer.() -> Unit = {}
+    abstract fun preDraw(code: Drawer.()->Unit)
+    override fun draw(program: Program) {
+        program.drawer._preDraw()
+
+    }
+}*/
 
 interface Costume {
     fun draw(state: SpriteState, program: Program)
 }
 
+interface Background : Drawable
+
 class ImageCostume(val buffer: ColorBuffer) : Costume {
     override fun draw(state: SpriteState, program: Program) {
-        program.drawer.image(buffer, position = state.position)
+        program.drawer.image(
+            buffer,
+            position = state.position,
+            width = buffer.width * state.size,
+            height = buffer.height * state.size
+        )
     }
 }
 
-class Sprite {
-    var costumes: List<Costume> = listOf()
-}
-
-fun <T> build(initial: T): (Builder<T>) -> T {
-    return fun(init: Builder<T>): T {
-        initial.init()
-        return initial
+class DrawerCostume(val drawCostume: Program.(SpriteState) -> Unit) : Costume {
+    override fun draw(state: SpriteState, program: Program) {
+        program.drawCostume(state)
     }
 }
 
-val sprite: (Builder<Sprite>) -> Sprite = build(Sprite())
+class TextCostume(val text: String, val font: Lazy<FontMap>, val preDraw: Drawer.() -> Unit) : Costume {
+    override fun draw(state: SpriteState, program: Program) = with(program) {
+        drawer.fontMap = font.value
+        drawer.preDraw()
+        drawer.text(text, state.position)
+    }
+}
 
-fun ktge(sprites: List<Sprite> /*background: BackgroundOptions*/) {
-    application(demoBuilder) // runs demo application for now
+class ImageBackground : Background {
+    val bg: Lazy<ColorBuffer>
+    val preDraw: BuildFun<Drawer>
+
+    constructor(image: ColorBuffer, preDraw: BuildFun<Drawer>) {
+        bg = lazy { image }
+        this.preDraw = preDraw
+    }
+
+    constructor(path: String, preDraw: BuildFun<Drawer>) {
+        bg = lazy { loadImage(path) }
+        this.preDraw = preDraw
+    }
+
+    override fun draw(program: Program) {
+        program.drawer.preDraw()
+        program.drawer.image(bg.value)
+    }
+}
+
+typealias SpriteStateFun = Program.(SpriteState) -> SpriteState
+
+//fun static(x: Double, y: Double): PositionLogic = { Vector2(x, y) }
+
+class Sprite(
+    val costumes: List<Costume>,
+    val costumeNames: Map<String, Int>,
+    val stateFun: SpriteStateFun,
+    initialState: SpriteState
+) : Drawable {
+    var state: SpriteState = initialState
+    var costumeNum: Int = 0
+
+    fun update(program: Program) {
+        state = program.stateFun(state)
+    }
+
+    override fun draw(program: Program) {
+        costumes[costumeNum].draw(state, program)
+    }
+}
+
+object StateFun {
+    fun fixed(x: Double, y: Double, size: Double = 1.0): SpriteStateFun =
+        { SpriteState(position = Vector2(x, y), size = size) }
+
+    fun position(posFun: Program.(SpriteState) -> Vector2): SpriteStateFun =
+        { SpriteState(position = posFun(it), size = it.size) }
+}
+
+class SpriteBuilder : Builder<Sprite> {
+    private val _costumes: MutableList<Costume> = mutableListOf()
+    private val _costumeNames: MutableMap<String, Int> = mutableMapOf()
+    var stateFun: SpriteStateFun = { it }
+    var initialState: SpriteState = SpriteState(Vector2.ZERO, 1.0)
+
+    fun costume(c: Costume, name: String? = null) {
+        name?.let { _costumeNames[it] = _costumes.size }
+        _costumes.add(c)
+    }
+
+    override fun build(): Sprite =
+        Sprite(costumes = _costumes, costumeNames = _costumeNames, stateFun = stateFun, initialState = initialState)
+}
+
+fun <T, B : Builder<T>> build(initial: () -> B): (BuildFun<B>) -> T {
+    return fun(init: BuildFun<B>): T {
+        val t = initial()
+        t.init()
+        return t.build()
+    }
+}
+
+val sprite: (BuildFun<SpriteBuilder>) -> Sprite = build(::SpriteBuilder)
+
+fun ktge(sprites: List<Sprite>, config: BuildFun<Configuration>, background: Background) {
+    //application(demoBuilder) // runs demo application for now
+    application {
+        configure(config)
+        program {
+            extend {
+                background.draw(this)
+                for (spr in sprites) {
+                    spr.update(this)
+                    spr.draw(this)
+                }
+            }
+        }
+    }
 }
--- src/main/kotlin/de/thecommcraft/ktgeDemo/Main.kt
+++ src/main/kotlin/de/thecommcraft/ktgeDemo/Main.kt
@@ -1,7 +1,41 @@
 package de.thecommcraft.ktgeDemo
 
-import de.thecommcraft.ktge.ktge
+import de.thecommcraft.ktge.*
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.loadFont
+import org.openrndr.draw.tint
+import org.openrndr.math.Vector2
+import kotlin.math.cos
+import kotlin.math.sin
+
+val pinkCircle = sprite {
+    costume(DrawerCostume {
+        drawer.fill = ColorRGBa.PINK
+        drawer.circle(it.position, 140.0)
+    })
+    stateFun = StateFun.position {
+        Vector2(
+            x = cos(seconds) * width / 2.0 + width / 2.0,
+            y = sin(0.5 * seconds) * height / 2.0 + height / 2.0
+        )
+    }
+}
+
+val openrndrText = sprite {
+    val font = lazy { loadFont("data/fonts/default.otf", 64.0) }
+    costume(TextCostume("OPENRNDR", font = font, preDraw = {
+        fill = ColorRGBa.WHITE
+    }))
+    stateFun = StateFun.position { Vector2(width / 2.0, height / 2.0) }
+}
 
 fun main() = ktge(
-    sprites = listOf()
+    sprites = listOf(pinkCircle, openrndrText),
+    config = {
+        width = 768
+        height = 576
+    },
+    background = ImageBackground(
+        "data/images/pm5544.png",
+        preDraw = { drawStyle.colorMatrix = tint(ColorRGBa.WHITE.shade(0.2)) })
 )
