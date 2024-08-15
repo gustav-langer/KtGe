d169f07afbf32fec76e81131ac4d2bd4c5c2064e
gustav-langer <ich.leake.nicht@meine.email.adresse>
2024-08-15 12:24:40 +0200

    Added a stop function to AudioPlayable, changed AudioGroup to also be playable

--- src/main/kotlin/de/thecommcraft/ktge/Audio.kt
+++ src/main/kotlin/de/thecommcraft/ktge/Audio.kt
@@ -6,6 +6,7 @@ import javax.sound.sampled.Clip
 
 interface AudioPlayable {
     fun play()
+    fun stop()
 }
 
 class Audio(file: File) : AudioPlayable, AutoCloseable {
@@ -22,6 +23,10 @@ class Audio(file: File) : AudioPlayable, AutoCloseable {
         _audioClip.start()
     }
 
+    override fun stop() {
+        _audioClip.stop()
+    }
+
     override fun close() {
         _audioClip.close()
     }
@@ -30,28 +35,44 @@ class Audio(file: File) : AudioPlayable, AutoCloseable {
 class AudioGroup(
     val audios: List<AudioPlayable>,
     val audioNames: Map<String, Int> // TODO replace this and the corresponding construct for Sprite Costumes with a better data type
-) {
-    fun selectAudio(audioName: String): AudioPlayable {
-        return audios[audioNames[audioName]!!]
+) : AudioPlayable {
+    private var selected: String? = null
+
+    fun selectAudio(name: String) {
+        selected = name
     }
 
     fun playAudio(audioName: String) {
-        selectAudio(audioName).play()
+        selected = audioName
+        this[audioName].play()
+    }
+
+    override fun play() {
+        selected?.let { this[it].play() }
+    }
+
+    override fun stop() {
+        selected?.let { this[it].stop() }
     }
 
     operator fun get(audioName: String): AudioPlayable {
-        return selectAudio(audioName)
+        return audios[audioNames[audioName]!!]
     }
 }
 
 class AudioGroupBuilder {
-    private val _audios: MutableList<Audio> = mutableListOf()
+    private val _audios: MutableList<AudioPlayable> = mutableListOf()
     private val _audioNames: MutableMap<String, Int> = mutableMapOf()
 
-    fun audio(filePath: String, name: String) {
+    fun audioFile(filePath: String, name: String) {
         val loadedAudio = Audio(File(filePath))
         _audioNames[name] = _audios.size
         _audios.add(loadedAudio)
+    }
+
+    fun audio(audio: AudioPlayable, name: String) {
+        _audioNames[name] = _audios.size
+        _audios.add(audio)
     }
 
     fun build(): AudioGroup =
