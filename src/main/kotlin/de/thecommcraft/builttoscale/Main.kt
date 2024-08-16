03218bc2d5abc10c3b474cda9d1503286c3f633b
gustav-langer <ich.leake.nicht@meine.email.adresse>
2024-08-16 21:52:38 +0200

    Added demo program which is supposed to become an entry for the GMTK game jam 2024.

--- src/main/kotlin/de/thecommcraft/builttoscale/Main.kt
+++ src/main/kotlin/de/thecommcraft/builttoscale/Main.kt
@@ -1,24 +1,46 @@
 package de.thecommcraft.builttoscale
 
 import de.thecommcraft.ktge.DrawerCostume
+import de.thecommcraft.ktge.Sprite
 import de.thecommcraft.ktge.ktge
 import de.thecommcraft.ktge.sprite
 import org.openrndr.color.ColorRGBa
+import org.openrndr.math.Vector2
 
 val ball = sprite {
+    val gravity = 0.2
+
     var size = 10.0
+    var vel: Vector2 = Vector2.ZERO // velocity
+
+    fun Sprite.updateVelocity() {
+        val bottomEdge = position.y + size
+        val belowGround = bottomEdge - window.size.y
+        if (belowGround < 0) {
+            vel = vel.copy(y = vel.y + gravity)
+        } else {
+            vel = vel.copy(y = -belowGround)
+            //position = position.copy(y = window.size.y - size)
+        }
+    }
 
     costume(DrawerCostume {
         drawer.fill = ColorRGBa.CYAN
         drawer.circle(it, size)
     })
 
-    frame {
-        position = mouse.position
+    init {
+        position = Vector2(x = width / 2.0, y = 0.0)
     }
 
-    on(mouse.scrolled) {
-        //window.
+    frame {
+        updateVelocity()
+        position += vel
+    }
+
+    on(mouse.buttonDown) {
+        vel = Vector2.ZERO
+        position = mouse.position
     }
 }
 
